53244ecdf56d1bc385aefa70eff081407fe57ce4
unknown <avillavicencio@gmail.com>
2026-01-06 23:04:35 +0100

    Fix core package for Vercel: export from src instead of dist

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -4,6 +4,8 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import { normalizeTransactions, RawMoralisTransaction } from '@wallet-ops-arbitrum/core';
 
+
+
 dotenv.config();
 
 const app = express();
